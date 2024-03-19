3947e0c9788a72bf5568bf3676c449d8548be332
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-03-19 15:06:44 -0300

    rotas publicas e privadas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,20 @@
-import { Route, Routes as RRDRoutes } from "react-router-dom";
+import { Route, Routes as RRDRoutes, Navigate } from "react-router-dom";
 import * as Pg from "../pages";
+import PublicRoutes from "./PublicRoute";
+import { PrivateRoutes } from "./PrivateRoute";
 
 export function Routes() {
   return (
     <RRDRoutes>
-      <Route path="/" element={<Pg.Login />} />
+      <Route path="/" element={<PublicRoutes />}>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Pg.Login />} />
+        <Route path="/cadastro" element={<Pg.Register />} />
+      </Route>
+      <Route path="/" element={<PrivateRoutes />}>
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Pg.Home />} />
+      </Route>
     </RRDRoutes>
   );
 }
