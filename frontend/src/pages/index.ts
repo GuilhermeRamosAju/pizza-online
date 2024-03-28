4b307687e8b52f0d0630ed27ae3405fa36085843
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-03-28 14:16:12 -0300

    novas paginas

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,8 +1,18 @@
 import Login from "./Login/Login";
 import Register from "./Register/Register";
 import Home from "./Home/Home";
-import PlaceOrder from "./PlaceOrder"
+import PlaceOrder from "./PlaceOrder";
 import Cart from "./Cart";
 import MyOrders from "./MyOrders";
-
-export { Login, Register, Home, PlaceOrder, Cart, MyOrders };
+import MostOrderedFlavors from "./MostOrderFlavors";
+import Menu from "./Menu";
+export {
+  Login,
+  Register,
+  Home,
+  PlaceOrder,
+  Cart,
+  MyOrders,
+  MostOrderedFlavors,
+  Menu,
+};
